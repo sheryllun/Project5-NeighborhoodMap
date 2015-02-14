3d28dd991df7560d1c1cd8ee7aa598ff2254f5da
sheryllun <sheryl.v.lun@gmail.com>
2015-02-14 17:26:16 -0500

    Adding info windows

--- js/app.js
+++ js/app.js
@@ -1,20 +1,21 @@
 function appViewModel() {
   var self = this;
-  var map, city;
+  var map, city, infowindow;
 
 // Initialize Google map
-  function initialize() {
+  function mapInitialize() {
     city = new google.maps.LatLng(38.906830, -77.038599);
     map = new google.maps.Map(document.getElementById('map-canvas'), {
           center: city,
           zoom: 14
         });
+    infowindow = new google.maps.InfoWindow();
     getGroupons();
   }
 
 // Use API to get locations and deals, store info in an array
   function getGroupons() {
-    var grouponUrl = "https://partner-api.groupon.com/deals.json?tsToken=afc14db65a41970c883ef994628f8dc96a743462&division_id=washington-dc&filters=category:food-and-drink&offset=0&limit=15";
+    var grouponUrl = "https://partner-api.groupon.com/deals.json?tsToken=US_AFF_0_203644_212556_0&division_id=washington-dc&filters=category:food-and-drink&offset=0&radius=10&limit=20";
 
     var grouponDeals = [];
 
@@ -23,36 +24,65 @@ function appViewModel() {
       dataType: 'jsonp',
       success: function(data) {
         console.log(data);
-        for(var i = 0; i < 15; i++) {
+        for(var i = 0; i < 20; i++) {
           var venueLocation = data.deals[i].options[0].redemptionLocations[0];
             if (venueLocation === undefined) continue;
           var venueName = data.deals[i].merchant.name;
-          var actualDeal = data.deals[i].announcementTitle;
-          var venueLat = venueLocation.lat;
-          var venueLon = venueLocation.lng;
-          grouponDeals.push({dealName: venueName, dealLat: venueLat, dealLon: venueLon, dealDeal: actualDeal});
+              venueLat = venueLocation.lat,
+              venueLon = venueLocation.lng,
+              gLink = data.deals[i].dealUrl,
+              gImg = data.deals[i].mediumImageUrl,
+              blurb = data.deals[i].pitchHtml,
+              address = venueLocation.streetAddress1,
+              city = venueLocation.city,
+              state = venueLocation.state,
+              zip = venueLocation.postalCode;
+
+          grouponDeals.push({
+                              dealName: venueName, 
+                              dealLat: venueLat, 
+                              dealLon: venueLon, 
+                              dealLink: gLink, 
+                              dealImg: gImg, 
+                              dealBlurb: blurb,
+                              dealAddress: address + "<br>" + city + ", " + state + " " + zip
+                            });
           }
         mapMarkers(grouponDeals);
       }
     });
   }
 
-// Create and place markers on the map based on data from API
+// Create and place markers and info windows on the map based on data from API
   function mapMarkers(array) {
     $.each(array, function(index, value) {
       var latitude = array[index].dealLat;
       var longitude = array[index].dealLon;
       var geoLocation = new google.maps.LatLng(latitude, longitude);
       var thisRestaurant = array[index].dealName;
+
+      var contentString = '<div id="infowindow">' +
+      '<img src="' + array[index].dealImg + '">' +
+      '<h2>' + array[index].dealName + '</h2>' +
+      '<p>' + array[index].dealAddress + '</p>' +
+      '<p><a href="' + array[index].dealLink + '" target="_blank">Click to view deal</a></p>' +
+      '<p>' + array[index].dealBlurb + '</p></div>';
+
       var marker = new google.maps.Marker({
         position: geoLocation,
         title: thisRestaurant,
         map: map
       });
+
+      google.maps.event.addListener(marker, 'click', function() {
+         infowindow.setContent(contentString);
+         infowindow.open(map, marker);
+       });
     });
   }
 
-  initialize();
+  mapInitialize();
+
 }
 
 ko.applyBindings(new appViewModel());
